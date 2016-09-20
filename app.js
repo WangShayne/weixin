aff6b787fe8530e0a62241239b9cd08506fd3115
WangShayne <1614565666@qq.com>
2016-09-20 10:06:07 +0800

    first

---
+++ app.js
@@ -0,0 +1,90 @@
+var fs = require('fs')
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var WechatAPI = require('wechat-api');
+
+var api = new WechatAPI('wxd3b6dc6c73e2e61d', '0257a41339180408957a5293b451f62f')
+api.getAccessToken(function (err, token) {
+    console.log(err);
+    console.log(token);
+});
+
+var img = "./src/img/head10.jpg";
+api.uploadMaterial(img,'image',function(err,result){
+  console.log(err)
+  console.log(result)
+})
+
+/*var mp3 = "./src/music/RolyPoly.mp3"
+api.uploadMaterial(mp3,'voice',function(err,result){
+  console.log(err)
+  console.log(result)
+})*/
+
+
+var menu = JSON.stringify(require("./src/json/menu.json"));
+api.createMenu(menu,function(err,result){
+  console.log(err)
+  console.log(result)
+})
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var weixin = require('./routes/weixin');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/weixin',weixin)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/weixin.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+var wechat = require('wechat');
+
+var config = {
+  token : 'weixin',
+  appid : 'wxd3b6dc6c73e2e61d',
+  appsecret :'0257a41339180408957a5293b451f62f',
+  encodingAESKey : ''
+};
+
+router.use(express.query());
+
+router.use('/', wechat(config, function(req, res, next) {
+    console.log(req.weixin);
+    var message = req.weixin;
+    //文本
+    if(message.MsgType === "event" ){
+      if(message.Event === "subscribe"){
+        res.reply({
+          type:"text",
+          content:"谢谢关注!<a href='http://shayne.tunnel.qydev.com/'>请访问我们的主页</a> "
+        });
+      }
+      if(message.Event === "CLICK"){
+          switch (message.EventKey){
+            case "A_text":
+              res.reply({
+                type:"text",
+                content:"返回文本消息"
+              })
+              break;
+            /*case "A_image":
+              res.reply({
+                type:"iamge",
+                content:{
+                  mediaId:"Nj3_EiWh4T-TwgGV1kmJIgT6wiThaEITKYJf0cXa38o"
+                }
+              })
+              break;*/
+            case "A_news":
+              res.reply([{
+                title:"图文混合消息",
+                description:"这块是简介",
+                picurl:"http://mmbiz.qpic.cn/mmbiz_jpg/frGYu2EeZNLoHsFTkKf9Jf3I3NFT0BaNxxEDaz7jn0FrhlZFLInF3m552Obm2GDmtkaAZCWQOsqwLmWVoayqfQ/0?wx_fmt=jpeg",
+                url:"https://www.baidu.com"
+              }])
+              break;
+            case "A_music":
+                res.reply({
+                  title: "音乐",
+                  description: "一无所有",
+                  musicUrl: "http://music.163.com/#/m/song?id=22704441&userid=90498156",
+                  hqMusicUrl: "http://music.163.com/#/m/song?id=22704441&userid=90498156",
+                  thumbMediaId: ""
+                });
+                break;
+          }
+      }
+      if(message.Event === "scancode_waitmsg"){
+        var scanType,scanResult;
+        message.ScanCodeInfo.ScanType === 'barcode' ? scanType = '条形码' : scanType = "二维码" ;
+        scanResult = message.ScanCodeInfo.ScanResult
+        res.reply({
+          type:"text",
+          content:"您扫的是"+scanType+"</br>值是"+scanResult
+        })
+      }
+    }
+
+    if(message.MsgType === "location"){
+      res.reply({
+        type:"text",
+        content:"您所在的位置是"+message.Label
+      });
+    }
+
+}));
+
+module.exports = router;
